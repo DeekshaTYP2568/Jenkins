a5004a0f5a379da634e14ce0e5a6f93896d2d1e6
Deeksha M <Deeksha M@TYSS-Deeksha-M>
2023-12-19 11:07:30 +0530

    first push

---
+++ src/test/java/jenkins/JenkinsMaven.java
@@ -0,0 +1,12 @@
+package jenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class JenkinsMaven {
+	
+	@Test
+	public void demo() {
+	Reporter.log("jenkins",true);	
+	}
+}
